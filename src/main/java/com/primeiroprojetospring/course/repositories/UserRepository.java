b374f07f6efb4e3ea485e95a56cda5565dedf31b
douglasNumeriano <douglas_rnmeriano@hotmail.com>
2020-12-27 10:50:47 -0300

    Service layer, component registration

--- src/main/java/com/primeiroprojetospring/course/repositories/UserRepository.java
+++ src/main/java/com/primeiroprojetospring/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.primeiroprojetospring.course.repositories;
 import com.primeiroprojetospring.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Neste caso não seria necessario usar uma anotação aqui pois por estar herdando da classe "JpaRepository"
 public interface UserRepository extends JpaRepository<User, Long> { // será uma interface (como se fosse o DAO do ERP do serviço)
 
 }
