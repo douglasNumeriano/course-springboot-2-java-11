32d661f82c459497d79544dbea64e100f2508ad4
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-17 17:27:34 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/primeiroprojetospring/course/entities/OrderItem.java
@@ -0,0 +1,76 @@
+package com.primeiroprojetospring.course.entities;
+
+import com.primeiroprojetospring.course.entities.pk.OrderItemPK;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name= "tb_order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantidade;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantidade, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantidade = quantidade;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
