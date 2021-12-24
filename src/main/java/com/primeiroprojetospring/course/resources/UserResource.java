b49f38bc711254b6e80520bcb7c700f6025cbe93
numeriano <douglas_rnmeriano@hotmail.com>
2021-12-24 12:00:23 -0300

    User update

--- src/main/java/com/primeiroprojetospring/course/resources/UserResource.java
+++ src/main/java/com/primeiroprojetospring/course/resources/UserResource.java
@@ -44,4 +44,9 @@ public class UserResource { // Recursos da entidade  User
         return  ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
